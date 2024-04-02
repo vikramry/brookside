9c2f86ef1aaf086b332af2bff4444471ea081a5a
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-04-02 16:35:17 +0530

    changes in responsive

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -2,14 +2,57 @@ import Banner from '@/components/AcademicsComponents/Banner'
 import ContactSection from '@/components/AcademicsComponents/ContactSection'
 import Curriculum from '@/components/AcademicsComponents/Curriculum'
 import MainContent from '@/components/AcademicsComponents/MainContent'
+import ActivityCard from '@/components/activityCard'
 import React from 'react'
+const data=[
+    {
+        content:'Confidence is an important factor in academic settings and can impact the student’s ability to learn and succeed. Students are engaged in classroom discussions where they are more likely to apply their skills and ask questions, which helps build confidence by providing them with constructive feedback. It builds positive reinforcement in students by developing a positive self-image and mindset necessary for success.',
+        heading:'Confidence',
+        imgURL:"/assets/images/activitycard1.png",
+        isReverse:false
 
+    },{
+        content:'Communication plays a crucial role in academic settings, where students, educators, and researchers are constantly exchanging ideas and information. We believe that encouraging students to ask questions, share their opinions, and participate in discussions can improve communication and help students in peer learning.',
+        heading:'Communication',
+        imgURL:"/assets/images/activitycard2.png",
+        isReverse:true
+
+    },
+    {
+        content:'Critical thinking is an essential skill for academic success, and integrating it with the curriculum helps students develop this ability. Students are encouraged to ask questions and identify information from various sources to develop a deeper understanding of a subject. Various opportunities to do research and inquiry-based project encourage students to gather, analyze and evaluate information.',
+        heading:'Critical thinking',
+        imgURL:"/assets/images/activitycard1.png",
+        isReverse:false
+
+    },
+    {
+        content:'Creativity encourages students to think critically, explore different perspectives, and express themselves in a creative way. Students use their creativity and imagination to explore different topics and work in a collaborative way. Integration of creativity in academics helps students become more successful in their academic pursuits.',
+        heading:'Creativity',
+        imgURL:"/assets/images/activitycard2.png",
+        isReverse:true
+
+    },
+    {
+        content:'Collaboration is an essential aspect of the school’s academic experience. Students work on group projects, where they collaborate with their classmates to complete a task or assignment. It not only develops teamwork but also enhances critical thinking and collaboration skills.',
+        heading:'Collaboration',
+        imgURL:"/assets/images/activitycard1.png",
+        isReverse:false
+
+    },
+]
 const page = () => {
     return (
         <div>
             <Banner />
             <MainContent />
             <Curriculum />
+            <div>
+        {data?.map((item)=>{
+            return(
+                <ActivityCard content={item?.content} heading={item?.heading} imgURL={item?.imgURL} isReverse={item?.isReverse}/>
+            )
+        })}
+        </div>
             <ContactSection />
         </div>
     )
--- src/components/activityCard.tsx
+++ src/components/activityCard.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function ActivityCard({heading,content,imgURL,isReverse}:{heading:string,content:string,imgURL?:string,isReverse?:boolean}) {
   return (
-    <div className={`${isReverse?"bg-gradient-to-r from-neutral-50 to-green-100":"bg-gradient-to-r from-blue-100 to-zinc-50"} py-11 px-36`}>
-        <div className={`flex justify-center ${isReverse ?"text-end flex-row-reverse ":"text-start flex-row"} items-center gap-9`}>
-            <div className='w-[50%]'>
-                <div className={`flex-col flex  ${isReverse&& "items-end"}`}>
-                <h1 className='text-[52px] font-bold'>{heading}</h1>
-                <div className='bg-[#F3A20C] w-[114px] h-2'></div>
+    <div className={`${isReverse?"bg-gradient-to-r from-neutral-50 to-green-100":"bg-gradient-to-r from-blue-100 to-zinc-50"} py-11 md:px-36 px-5`}>
+        <div className={`flex justify-center ${isReverse ?"text-end md:flex-row-reverse flex-col-reverse":"text-start md:flex-row flex-col"} items-center gap-9`}>
+            <div className='md:w-[50%]'>
+                <div className={`flex-col flex  ${isReverse&& "items-end"} gap-2`}>
+                <h1 className='md:text-[30px] lg:text-[52px] text-[20px] font-bold'>{heading}</h1>
+                <div className='bg-[#F3A20C] md:w-[114px] w-[50px] md:h-2 h-1 mb-3'></div>
                 </div>
                 <p className='text-[16px] font-normal'>{content}</p>
             </div>
