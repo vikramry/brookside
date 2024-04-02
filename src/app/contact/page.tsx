78f81feebb79db13331d551b4f612d3c9280fb39
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-04-02 17:17:28 +0530

    changes in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 const page = () => {
     return (
-        <div>
+        <div className="bg-white">
+                  <div>
+
             <div>
                 <img src="/assets/images/contactbg.png" />
             </div>
-            <div className="p-20 bg-white">
-                <div className="shadow-xl bg-white p-20 rounded-2xl flex flex-row flex-wrap gap-x-28 justify-center">
+            <div className="md:p-20 bg-white">
+                <div className="shadow-xl bg-white md:p-20 p-10 rounded-2xl flex flex-row flex-wrap md:gap-28 gap-10 justify-center ">
                     <div className="flex flex-col gap-y-5" >
                         <h1 className="text-[40px] font-semibold">We’d love to hear from you</h1>
                         <input type="text" placeholder="Your name*" className="border-2 bg-gray-50 p-2 rounded" />
@@ -21,10 +23,12 @@ const page = () => {
                         <button className="bg-[#F3A20C] rounded-md p-2">SEND YOUR MESSAGE</button>
 
                     </div>
-                    <div className="mapouter"><div className="gmap_canvas"><iframe className="rounded-2xl" width="460" height="535" id="gmap_canvas" src="https://maps.google.com/maps?q=The%20Brookfield%20International%20school%20kondapur&t=&z=13&ie=UTF8&iwloc=&output=embed" scrolling="no" ></iframe></div></div>
+                    <div className="mapouter"><div className="gmap_canvas"><iframe className="rounded-2xl md:w-[460px] md:h-[535px]  h-[500px]" id="gmap_canvas" src="https://maps.google.com/maps?q=The%20Brookfield%20International%20school%20kondapur&t=&z=13&ie=UTF8&iwloc=&output=embed" scrolling="no" ></iframe></div></div>
 
                 </div>
             </div>
+            </div>
+
         </div>
     )
 }
