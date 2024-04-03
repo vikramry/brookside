ed83baa681517dbfc663b8d504f54f5db6adfb3a
thrinetra <thrinetravikram@gmail.com>
2024-04-03 15:56:39 +0530

    changes in about page

--- src/components/AboutGlance.tsx
+++ src/components/AboutGlance.tsx
@@ -6,19 +6,19 @@ const AboutGlance = () => {
             <h1 className='text-[#F3A20C] font-bold md:text-5xl text-2xl '>Brookfield at a Glance</h1>
             <div className='flex justify-center items-start flex-col gap-20'>
                 <div className='flex justify-start items-center gap-10'>
-                    <span className='text-3xl'>Current Admissions</span>
-                    <hr className='w-60 border-[1px] border-[#F3A20C]' />
-                    <span className='text-5xl'>200</span>
+                    <span className='md:text-3xl text-[16px] '>Current Admissions</span>
+                    <hr className='md:w-60 w-10 border-[1px] border-[#F3A20C]' />
+                    <span className='md:text-5xl text-[30px]'>200</span>
                 </div>
-                <div className='flex justify-start items-center gap-20'>
-                    <span className='text-3xl'>Qualified Staff</span>
-                    <hr className='w-60 border-[1px] border-[#F3A20C]' />
-                    <span className='text-5xl'>20+</span>
+                <div className='flex justify-start items-center md:gap-20 gap-12'>
+                    <span className='md:text-3xl text-[16px] '>Qualified Staff</span>
+                    <hr className='md:w-60 w-10 border-[1px] border-[#F3A20C]' />
+                    <span className='md:text-5xl text-[30px]'>20+</span>
                 </div>
                 <div className='flex justify-start items-center gap-12'>
-                    <span className='text-3xl'>Clubs & Activities</span>
-                    <hr className='w-60 border-[1px] border-[#F3A20C]' />
-                    <span className='text-5xl'>30+</span>
+                    <span className='md:text-3xl text-[16px]'>Clubs & Activities</span>
+                    <hr className='md:w-60 w-10 border-[1px] border-[#F3A20C]' />
+                    <span className='md:text-5xl text-[30px]'>30+</span>
                 </div>
             </div>
         </div>
