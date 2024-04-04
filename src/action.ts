e1dd98df5e801840c6265cb3f1b4e683cac3af68
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-04-04 16:31:31 +0530

    added filters to gallery, pulling filters from cloudinary

--- src/action.ts
+++ src/action.ts
@@ -155,9 +155,9 @@ export async function AdmissionMail(data: any) {
 }
 
 
-export const getGalleryImagesFromCloudinary = async () => {
+export const getGalleryImagesFromCloudinary = async (folder: string) => {
   const cloudName = process.env.CLOUDINARY_CLOUD_NAME
-  const cloudinaryEndpoint = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image?asset_folder=brookside-gallery&max_results=100`;
+  const cloudinaryEndpoint = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image?asset_folder=brookside-gallery&max_results=500`;
 
   const apiKey = process.env.CLOUDINARY_API_KEY;
   const apiSecret = process.env.CLOUDINARY_API_SECRET;
@@ -173,6 +173,30 @@ export const getGalleryImagesFromCloudinary = async () => {
     }
   })
   const data = await respone.json();
-  
-  return JSON.stringify(data?.resources?.filter((resource: any) => resource?.folder === "brookside-gallery"));
+  const photos = data?.resources?.filter((resource: any) => folder === "ALL" ? resource?.folder.includes("brookside-gallery") : resource?.folder === `brookside-gallery/${folder.charAt(0) + folder.slice(1).toLowerCase()}`)
+  return JSON.stringify(photos);
+}
+
+
+export const getAllFoldersFromCloudinary = async () => {
+  const cloudName = process.env.CLOUDINARY_CLOUD_NAME
+  const cloudinaryEndpoint = `https://api.cloudinary.com/v1_1/${cloudName}/folders/brookside-gallery`;
+
+  const apiKey = process.env.CLOUDINARY_API_KEY;
+  const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+  const credentials = `${apiKey}:${apiSecret}`;
+  const encodedCredentials = btoa(credentials);
+  const authorizationHeader = `Basic ${encodedCredentials}`;
+
+  const respone = await fetch(cloudinaryEndpoint, {
+    method: "GET",
+    headers: {
+      'Authorization': authorizationHeader
+    }
+  })
+  const data = await respone.json();
+  const tabs = data?.folders?.map((fold: any) => { return { label: fold?.name, value: fold?.name?.toUpperCase() } });
+  return JSON.stringify([{ label: "All", value: "ALL" }, ...tabs]);
+
 }
--- src/components/photos.tsx
+++ src/components/photos.tsx
@@ -3,8 +3,8 @@ import { getGalleryImagesFromCloudinary } from "@/action";
 const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
 
 
-async function loadPhotos() {
-    const cloudinaryImages = await getGalleryImagesFromCloudinary();
+async function loadPhotos(folder: string) {
+    const cloudinaryImages = await getGalleryImagesFromCloudinary(folder);
     const photos = JSON.parse(cloudinaryImages).map((photo: any) => {
         return {
             src: photo.secure_url, // Use the secure_url from Cloudinary response
