e5b4d3d3f0859f3107b4fed92a40de1ee9c9d55e
yaswanthnarisetty <yaswanthyuga@gmail.com>
2024-04-19 10:52:49 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.brooksideschool.in',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
