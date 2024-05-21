f2189e5ad71e95f990a921e5d72d61cccf160258
Nishasahoo <nisha.sahoo99@gmail.com>
2024-05-21 12:51:11 +0530

    metatittle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,9 @@ import ContactSection from "@/components/AcademicsComponents/ContactSection";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Best Primary School in Hyderabad",
-  description: "Welcome to Brookside Global School, where young minds bloom! We are the Best Primary School in Hyderabad. Join us on an exciting educational journey tailored to inspire and empower our students for a lifetime of success.",
-  keywords:"Best Primary School in Hyderabad, Best Pre Primary Schools in Hyderabad"
+  title: "Best Primary School in Kondapur, Hyderabad",
+  description: "Welcome to Brookside Global School, where young minds bloom! We are the Best Primary School in Kondapur, Hyderabad. Join us on an exciting educational journey tailored to inspire and empower our students for a lifetime of success.",
+  keywords:"Best Primary School in Kondapur - Hyderabad, Best Primary School in Hyderabad, Best Pre Primary Schools in Hyderabad'"
 };
 
 export default function Home() {
