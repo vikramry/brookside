aab14f46eae4af046947e40d169a7548a9bd2c68
Vikram <vikramr@vithiit.com>
2024-05-27 19:28:38 +0530

    gtm update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="google-site-verification" content="g0qhvJ6n-eHI9d3V4D78bBwCYJALhVgHgYs1Se75AUI" />
+      
+
       <body className="flex justify-center items-center">
         <Header />
         <div className="flex min-h-screen flex-col w-full max-w-screen-2xl justify-center">
@@ -29,6 +32,7 @@ export default function RootLayout({
           <Footer />
         </div>
       </body>
+      <GoogleTagManager gtmId="AW-16583727033" />
     </html>
   );
 }
